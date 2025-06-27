f319a7f3db2045905f9d7d69a5c3c9a5935e488d
Luka <62384447+fluxyn@users.noreply.github.com>
2025-06-27 17:56:32 -0400

    supercool: Add reload function

--- src/supercool.js
+++ src/supercool.js
@@ -4,23 +4,23 @@ export default class supercool {
     constructor(dir, renderingMethod) {
         this.dir = dir
         this.renderingMethod = renderingMethod
+        this.reload()
     }
 
-    get(path) {
+    reload() {
         let tags, state, router, body
 
         eval(fs.readFileSync('src/ssr.js', 'utf8'))
 
-        let script
         if (this.renderingMethod === 'csr' || this.renderingMethod === 'hydration')
-            script = fs.readFileSync('src/csr.js', 'utf8') + '\n'
+            this.script = fs.readFileSync('src/csr.js', 'utf8') + '\n'
         
         fs.readdirSync(this.dir).forEach(file => {
             if (fs.statSync(this.dir + file).isFile() && file.endsWith('.js')) {
                 const content = fs.readFileSync(this.dir + file, 'utf8')
 
                 if (this.renderingMethod === 'csr' || this.renderingMethod === 'hydration')
-                    script += content + '\n'
+                    this.script += content + '\n'
                 
                 if (this.renderingMethod === 'ssr' || this.renderingMethod === 'hydration')
                     eval(content)
@@ -28,17 +28,25 @@ export default class supercool {
         })
         
         if (this.renderingMethod === 'ssr' || this.renderingMethod === 'hydration')
-            router.goto(path)
+            this.router = router
+            this.body = body
 
         //if (this.renderingMethod === 'csr' || this.renderingMethod === 'hydration')
             // TODO run script through bundler/minifier
+    }
 
-        if (this.renderingMethod === 'csr')
-            return `<html><head><script>${script}</script></head><body></body></html>`
-        else if (this.renderingMethod === 'ssr')
-            return `<html><head></head><body>${body}</body></html>`
-        else if (this.renderingMethod === 'hydration')
-            return `<html><head><script>${script}</script></head><body>${body}</body></html>`
+    get(path, reload = false) {
+        if (reload) reload()
+        
+        if (this.renderingMethod === 'csr') {
+            return `<html><head><script>${this.script}</script></head><body></body></html>`
+        } else if (this.renderingMethod === 'ssr') {
+            this.router.goto(path)
+            return `<html><head></head><body>${this.body}</body></html>`
+        } else if (this.renderingMethod === 'hydration') {
+            this.router.goto(path)
+            return `<html><head><script>${this.script}</script></head><body>${this.body}</body></html>`
+        }
     }
 
     handle(req, res) {
